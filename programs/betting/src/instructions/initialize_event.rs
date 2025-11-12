c7c2f319da4593eb31807f26bcbc4e1802ec918d
MateuszWisniewskii <mdw180703@gmail.com>
2025-11-12 16:14:50 +0100

    dodanie odpowiednich ograniczeń dla kont w InitializeEvent, oraz dla pól w EventAccount

--- programs/betting/src/instructions/initialize_event.rs
+++ programs/betting/src/instructions/initialize_event.rs
@@ -3,17 +3,37 @@ use anchor_lang::prelude::*;
 use crate::EventAccount;
 
 #[derive(Accounts)]
+#[instruction(event_id: u64)]
 pub struct InitializeEvent<'info> {
-    #[account()]
-    pub authority: Signer<'info>,
+    #[account(mut)]
+    pub authority: Signer<'info>, // admin bądź inna jednostka autoryzująca wydarzenie
 
-    #[account()]
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + EventAccount::INIT_SPACE,
+        seeds = [b"event_seed".as_ref(), event_id.to_le_bytes().as_ref()],
+        bump
+    )]
     pub event_account: Account<'info, EventAccount>,
 
     pub system_program: Program<'info, System>,
 }
 
-pub fn handler(ctx: Context<InitializeEvent>) -> Result<()> {
-    msg!("Greetings from: {:?}", ctx.program_id);
+pub fn handler(
+    ctx: Context<InitializeEvent>,
+    event_id: u64,
+    start_time: u64,
+    end_time: u64,
+    event_name: String,
+    event_description: String,
+) -> Result<()> {
+    //msg!("Greetings from: {:?}", ctx.program_id);
+    ctx.accounts.event_account.event_name = event_name;
+    ctx.accounts.event_account.event_description = event_description;
+    ctx.accounts.event_account.betting_start = start_time;
+    ctx.accounts.event_account.betting_end = end_time;
+    ctx.accounts.event_account.betting_options_index = 0;
+    ctx.accounts.event_account.event_resolved = false;
     Ok(())
 }
--- programs/betting/src/state/event_account.rs
+++ programs/betting/src/state/event_account.rs
@@ -1,11 +1,14 @@
 use anchor_lang::prelude::*;
 
 #[account]
+#[derive(InitSpace)]
 pub struct EventAccount {
+    #[max_len(40)]
     pub event_name: String, // nazwa wydarzenia np. drużyna A vs drużyna B
+    #[max_len(200)]
     pub event_description: String, // opis wydarzenia
     pub betting_start: u64, // moment rozpoczęcia możliwości obstawiania
     pub betting_end: u64, // moment zakończenia możliwości obstawiania
-    pub betting_options_indeks: u64, // liczba opisująca ilość kandydatów/opcji do głosowania (w przypadku piłki nożnej będą tylko dwie opcje)
+    pub betting_options_index: u64, // liczba opisująca ilość kandydatów/opcji do głosowania (w przypadku piłki nożnej będą tylko dwie opcje)
     pub event_resolved: bool, // zmienna warunkująca, kiedy wydarzenie się skończyło i można odbierać nagrodę 
 }
